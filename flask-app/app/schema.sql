fa2da2254146ec8c0ea78973001a9ea0fbb9ff47
eyjhbb@gmail.com <eyjhbb@gmail.com>
2018-06-21 16:26:49 +0200

    removed old server

--- flask-app/app/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists readings;
-create table readings (
-    id integer primary key autoincrement,
-    name text not null,
-    reading_date DATETIME not null,
-    sent_value float not null,
-    recv_value float not null
-);
--- flask-app/app/templates/readings.js
+++
@@ -1,8 +0,0 @@
-var readings = [
-{%for reading in readings%}
-  <tr>
-    <td>{{reading[0]}}</td>
-  </tr>   
-{%endfor%}
-
-];
